714d87f63b6dea6a35f4712dbaf1f23ae29d63bf
xinyi200622 <2502939835@qq.com>
2026-01-17 20:32:59 +0800

    ValidateInput组件完成

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,24 @@
 <template>
   <div class="container">
     <global-header :user="currentUser"></global-header>
-    <column-list :list="list"></column-list>
+    <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Email address</label>
+    <validate-input :rules="emailRules" v-model="emailVal" placeholder="请输入邮箱地址"></validate-input>{{ emailVal }}
+    <div class="form-text" v-if="emailRef.error">{{ emailRef.message }}</div>
+    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Password</label>
+    <validate-input type="password" placeholder="请输入密码" :rules="passwordRules" v-model="passwordVal"></validate-input>
+  </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, reactive, ref } from 'vue'
 import '../bootstrap-5.3.0-alpha1-dist/css/bootstrap.min.css'
 import '../bootstrap-5.3.0-alpha1-dist/js/bootstrap.min.js'
+import ValidateInput, { RulesProp } from './components/ValidateInput.vue'
 import ColumnList, { ColumnProps } from './components/ColumnList.vue'
 import GlobalHeader, { UserProps } from './components/GlobalHeader.vue'
 const currentUser: UserProps = {
@@ -44,13 +54,40 @@ const testData: ColumnProps[] = [
 export default defineComponent({
   name: 'App',
   components: {
-    ColumnList,
-    GlobalHeader
+    GlobalHeader,
+    ValidateInput
   },
   setup () {
+    const emailVal = ref('')
+    const emailRules: RulesProp = [
+      { type: 'required', message: '电子邮箱地址不能为空' },
+      { type: 'email', message: '请输入正确的电子邮箱格式' }
+    ]
+    const emailRef = reactive({
+      val: '',
+      error: false,
+      message: ''
+    })
+    const passwordVal = ref('')
+    const passwordRules: RulesProp = [
+      { type: 'required', message: '密码不能为空' },
+      { type: 'password', message: '请输入长度 8-20 位，包含大写字母 + 小写字母 + 数字 + 特殊符号（至少各 1 位），排除空格的密码' }
+    ]
+    const passwordRef = reactive({
+      val: '',
+      error: false,
+      message: ''
+    })
     return {
       list: testData,
-      currentUser
+      currentUser,
+      emailRef,
+      ValidateInput,
+      emailRules,
+      emailVal,
+      passwordVal,
+      passwordRules,
+      passwordRef
     }
   }
 })
---
+++ src/components/ValidateInput.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="validate-input-container pb-3">
+  <input  class="form-control"
+          :class="{'is-invalid': inputRef.error}"
+          :value="inputRef.val"
+           @blur="validateInput"
+           @input="updateValue"
+           vind="$attrs">
+  <span v-if="inputRef.error" class="invalid-feedback">{{ inputRef.message }}</span>
+</div>
+</template>
+
+<script lang="ts">
+import { reactive, PropType, defineComponent } from 'vue'
+const emailReg = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+const passwordReg = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?])[a-zA-Z\d!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]{8,20}$/
+interface RuleProp {
+  type:'required' | 'email'| 'password';
+  message:string
+}
+export type RulesProp = RuleProp[]
+export default defineComponent({
+  props: {
+    rules: Array as PropType<RulesProp>,
+    modelValue: String
+  },
+  inheritAttrs: false,
+  setup (props, context) {
+    const inputRef = reactive({
+      val: props.modelValue || '',
+      error: false,
+      message: ''
+    })
+    const updateValue = (e: Event) => {
+      const targetValue = (e.target as HTMLInputElement).value
+      inputRef.val = targetValue
+      context.emit('update:modelValue', targetValue)
+    }
+    const validateInput = () => {
+      console.log(context.attrs)
+      if (props.rules) {
+        const allPassed = props.rules.every(rule => {
+          let passed = true
+          inputRef.message = rule.message
+          switch (rule.type) {
+            case 'required':
+              passed = (inputRef.val.trim() !== '')
+              break
+            case 'email':
+              passed = emailReg.test(inputRef.val)
+              break
+            case 'password':
+              passed = passwordReg.test(inputRef.val)
+              break
+            default :
+              break
+          }
+          return passed
+        })
+        inputRef.error = !allPassed
+      }
+    }
+    return {
+      inputRef,
+      validateInput,
+      updateValue
+    }
+  }
+})
+</script>
